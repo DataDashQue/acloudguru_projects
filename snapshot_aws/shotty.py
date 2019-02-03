ce367265c7a08ba197fb0dede5daf2a5b707dabf
Datadash <victorkevin.quiambao@gmail.com>
2019-02-03 16:12:39 -0500

    Update the scrpit to use the click module and show more information for each instance

--- snapshot_aws/shotty.py
+++ snapshot_aws/shotty.py
@@ -1,8 +1,32 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>")
+
+def list_instances(project):
+    "List EC2 instances"
+    instances = []
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name
+        )))
+
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
