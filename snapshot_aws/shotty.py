27a4271a29c4eeb6d2696fe0bc6fb1969b0e5398
Datadash <victorkevin.quiambao@gmail.com>
2019-02-07 17:22:50 -0500

    Don't start a new snapshot when one is already pending

--- snapshot_aws/shotty.py
+++ snapshot_aws/shotty.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0] == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -95,6 +99,8 @@ def create_snapshot(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print(' Skipping {0}, snapshot already in progress'.format(v.id))
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer 5000")
         i.start()
