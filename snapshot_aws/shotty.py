6af1c983d4d21b6bdae618fe78b9ef953dbd4703
Datadash <victorkevin.quiambao@gmail.com>
2019-02-07 16:40:50 -0500

    List volumes and snapshots. Maive implmentation of snapshot command

--- snapshot_aws/shotty.py
+++ snapshot_aws/shotty.py
@@ -19,18 +19,95 @@ def filter_instances(project):
 def cli():
     """Shotty manages snapshots"""
 
+@cli.group('snapshot')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<name>")     
+def list_snapshots(project):
+    "List EC2 Snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+
 @cli.group('volumes')
 def volumes():
     """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<name>")     
+def list_volumes(project):
+    "List EC2 Volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+
 
 
 @cli.group('instances')
 def instances():
     """Commands for instances"""
 
+@instances.command('snapshot',
+    help="Create Snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshot(project):
+    "Create Snapshots for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by SnapshotAlyzer 5000")
+ 
+#    print("Stopping {0}...".format(i.id))
+ 
+#    i.stop()
+ #   i.wait_until_stopped()
+ 
+#    for v in i.volumes.all():
+#        print("Creating snapshot of {0}".format(v.id))
+#        v.create_snapshot(Description="Created by SnapshotAlyzer 5000")
+ 
+#    i.start()
+#    i.wait_until_running
+    
+#    print("Job has completed!")
+
+    return
+
 @instances.command('list')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>")
+
 
 def list_instances(project):
     "List EC2 instances"
@@ -67,7 +144,7 @@ def stop_instances(project):
 @instances.command('start')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>")
-def stop_instances(project):
+def start_instances(project):
     "Start EC2 Instances"
 
     instances = filter_instances(project)
@@ -77,6 +154,8 @@ def stop_instances(project):
         i.start()
     
     return
+
+
 
 if __name__ == '__main__':
     cli()
